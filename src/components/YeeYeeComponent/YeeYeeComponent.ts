eb53b8b5496230c29f01ed4fef2d1560c76e972a
Unknown <ali.majed.am@hotmail.se>
2019-03-02 20:58:37 +0100

    render lit with style

--- src/components/YeeYeeComponent/YeeYeeComponent.ts
+++ src/components/YeeYeeComponent/YeeYeeComponent.ts
@@ -1,12 +1,33 @@
-import { TemplateResult, render } from 'lit-html';
+import { TemplateResult, render, html } from 'lit-html';
 
 abstract class YeeYeeComponent extends HTMLElement {
-  constructor() {
+  yeeStyle: string;
+
+  constructor(yeeStyle?: string) {
     super();
 
+    this.yeeStyle = yeeStyle;
     this.attachShadow({ mode: 'open' });
+
     const litTemplate = this.render();
-    render(litTemplate, this.shadowRoot);
+
+    this.litRender(litTemplate);
+  }
+
+  private litRender(litTemplate: TemplateResult) {
+    const template = html`
+      ${this.getStyle(this.yeeStyle)} ${litTemplate}
+    `;
+
+    render(template, this.shadowRoot);
+  }
+
+  private getStyle(style: string): TemplateResult {
+    return html`
+      <style>
+        ${this.yeeStyle}
+      </style>
+    `;
   }
 
   protected abstract render(): TemplateResult;

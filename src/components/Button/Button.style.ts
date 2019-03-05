0a02c88a66a8ca073a928013e190fd2c6dc4892b
Unknown <ali.majed.am@hotmail.se>
2019-03-05 21:21:20 +0100

    ripple effect on button

--- src/components/Button/Button.style.ts
+++ src/components/Button/Button.style.ts
@@ -1,6 +1,7 @@
 import { defaultFontStyle } from '../../style/font.style';
 import { paddingStyle } from '../../style/padding.style';
 import { colorStyle } from '../../style/color.style';
+import { css } from '../../style/utilsStyle';
 
 const values = {
   normal: `
@@ -11,7 +12,7 @@ const values = {
   `,
 };
 
-const defaultButtonStyle: string = `
+const defaultButtonStyle = css`
   button {
     ${values.normal}
     font-family: ${defaultFontStyle.fontFamily}
@@ -34,4 +35,29 @@ const defaultButtonStyle: string = `
   }
 `;
 
-export { defaultButtonStyle };
+const rippleDurationMilli = 600;
+
+const ripple = css`
+  .rippleContainer {
+    position: relative;
+    overflow: hidden;
+    transform: translate3d(0, 0, 0);
+  }
+
+  .ripple {
+    position: absolute;
+    border-radius: 50%;
+    background-color: rgba(255, 255, 255, 0.3);
+    transform: scale(0);
+    animation: ripple ${rippleDurationMilli / 1000 + 's'} linear;
+  }
+
+  @keyframes ripple {
+    to {
+      transform: scale(2);
+      opacity: 0;
+    }
+  }
+`;
+
+export { defaultButtonStyle, ripple, rippleDurationMilli };
--- src/components/Button/Button.ts
+++ src/components/Button/Button.ts
@@ -1,16 +1,41 @@
 import { html, TemplateResult } from 'lit-html';
 import YeeYeeComponent from '../YeeYeeComponent';
-import { defaultButtonStyle } from './Button.style.ts';
+import {
+  defaultButtonStyle,
+  ripple,
+  rippleDurationMilli,
+} from './Button.style.ts';
 
 class Button extends YeeYeeComponent {
   constructor() {
-    super(defaultButtonStyle);
+    super(defaultButtonStyle + ripple);
   }
 
   protected render(): TemplateResult {
     return html`
-      <button><slot>NO LABEL</slot></button>
+      <button class="rippleContainer" @click=${this.onClick}>
+        <slot>NO LABEL</slot>
+      </button>
     `;
+  }
+
+  private onClick(e: MouseEvent): void {
+    const diameter = Math.max(this.clientWidth, this.clientHeight);
+    const rect = this.getBoundingClientRect();
+
+    const circle = document.createElement('div');
+
+    circle.style.width = circle.style.height = diameter + 'px';
+    circle.style.left = e.clientX - rect.left - diameter / 2 + 'px';
+    circle.style.top = e.clientY - rect.top - diameter / 2 + 'px';
+
+    circle.classList.add('ripple');
+
+    this.appendChild(circle);
+
+    setTimeout(() => {
+      this.removeChild(circle);
+    }, rippleDurationMilli);
   }
 }
 

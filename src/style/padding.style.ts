652a6ba0cd0f191b2a53f42029c3a3913ada4e72
Unknown <ali.majed.am@hotmail.se>
2019-03-24 11:07:22 +0100

    paddings

--- src/style/padding.style.ts
+++ src/style/padding.style.ts
@@ -1,5 +1,13 @@
 const paddingStyle = {
-  normal: '16px',
+  xxxSmall: '2px',
+  xxSmall: '4px',
+  xSmall: '6px',
+  small: '8px',
+  medium: '10px',
+  large: '12px',
+  xLarge: '14px',
+  xxLarge: '16px',
+  xxxLarge: '18px',
 };
 
 export { paddingStyle };

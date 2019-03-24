34fcdd06474ae3277da1049b64d0bc17d8944390
Unknown <ali.majed.am@hotmail.se>
2019-03-24 11:11:03 +0100

    IconButton: update entry point

--- webpack/entryPoints.js
+++ webpack/entryPoints.js
@@ -8,7 +8,7 @@ const entryPoints = {
   'yeeyee-button': path.resolve(BasePaths.YEEYEE_COMPONENTS, 'Button'),
   'yeeyee-navbar': path.resolve(BasePaths.YEEYEE_COMPONENTS, 'Navbar'),
   'yeeyee-icon': path.resolve(BasePaths.YEEYEE_COMPONENTS, 'Icon'),
-  'yeeyee-button-icon': path.resolve(
+  'yeeyee-button-icon': path.resolve(BasePaths.YEEYEE_COMPONENTS, 'IconButton'),
     BasePaths.YEEYEE_COMPONENTS,
     'Button',
     'IconButton'

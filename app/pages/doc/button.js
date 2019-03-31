c6e59431e6fe21eebb5dca02b2ef0329e630feac
Unknown <ali.majed.am@hotmail.se>
2019-03-31 15:40:48 +0200

    App: ButtonDoc

---
+++ app/pages/doc/button.js
@@ -0,0 +1,7 @@
+import ButtonDoc from '../../src/pages/doc/ButtonDoc';
+
+export default () => (
+  <React.Fragment>
+    <ButtonDoc />
+  </React.Fragment>
+);
---
+++ app/src/pages/doc/ButtonDoc/ButtonDoc.jsx
@@ -0,0 +1,19 @@
+import GlobalStyle from '../../../style/GlobalStyle';
+import Head from 'next/head';
+
+function ButtonDoc() {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Button</title>
+      </Head>
+      <GlobalStyle />
+      <yeeyee-navbar name="Button" url="/">
+        <yeeyee-drawer-item>Home</yeeyee-drawer-item>
+        <yeeyee-drawer-item>Documentation</yeeyee-drawer-item>
+      </yeeyee-navbar>
+    </React.Fragment>
+  );
+}
+
+export default ButtonDoc;

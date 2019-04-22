4f4aeaac552cf15dfee82779c6bd9779336c52b5
Ali <ali.majed.am@hotmail.se>
2019-04-22 11:40:11 +0200

    App: setup less

--- app/next.config.js
+++ app/next.config.js
@@ -1,4 +1,10 @@
-module.exports = {
+const withLess = require('@zeit/next-less');
+
+module.exports = withLess({
+	cssModules: true,
+	cssLoaderOptions: {
+		localIdentName: '[folder]___[name]__[local]___[hash:base64:5]',
+	},
 	webpack: (config, { dev }) => {
 		if (dev) {
 			config.module.rules.push({
@@ -9,4 +15,4 @@ module.exports = {
 		}
 		return config;
 	},
-};
+});

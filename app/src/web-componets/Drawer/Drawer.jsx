735f86e18c0c3d10139e9bdbb33fd3073b1985b5
Unknown <ali.majed.am@hotmail.se>
2019-04-27 10:23:11 +0200

    App: eslint-fix: Drawer wrapper

--- app/src/web-componets/Drawer/Drawer.jsx
+++ app/src/web-componets/Drawer/Drawer.jsx
@@ -1,25 +1,33 @@
+import PropTypes from 'prop-types';
 import { useRef, useEffect } from 'react';
 
 function Drawer(props) {
-  const drawerRef = useRef();
+	const drawerRef = useRef();
 
-  useEffect(() => {
-    if (Boolean(props.open)) {
-      drawerRef.current.setAttribute('open', '');
-    } else {
-      drawerRef.current.removeAttribute('open');
-    }
-  }, [props.open]);
+	useEffect(() => {
+		if (props.open) {
+			drawerRef.current.setAttribute('open', '');
+		} else {
+			drawerRef.current.removeAttribute('open');
+		}
+	}, [props.open]);
 
-  return (
-    <yeeyee-drawer
-      ref={drawerRef}
-      align={props.align}
-      offset-top={props.offsetTop}
-    >
-      {props.children}
-    </yeeyee-drawer>
-  );
+	return (
+		<yeeyee-drawer
+			ref={drawerRef}
+			align={props.align}
+			offset-top={props.offsetTop}
+		>
+			{props.children}
+		</yeeyee-drawer>
+	);
 }
+
+Drawer.propTypes = {
+	open: PropTypes.bool.isRequired,
+	align: PropTypes.string,
+	offsetTop: PropTypes.string,
+	children: PropTypes.oneOfType([PropTypes.node, PropTypes.array]).isRequired,
+};
 
 export default Drawer;

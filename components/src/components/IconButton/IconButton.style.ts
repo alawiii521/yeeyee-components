8bcef6e59eabf79e5626fc5c1b958bac078017c3
Unknown <ali.majed.am@hotmail.se>
2019-04-28 13:25:55 +0200

    Component: bugfix: IconButton floating css

--- components/src/components/IconButton/IconButton.style.ts
+++ components/src/components/IconButton/IconButton.style.ts
@@ -41,8 +41,10 @@ const type = (type: string): string => {
 const floating = (floating: boolean): string => {
 	if (floating) {
 		return css`
-			box-shadow: 0px 3px 5px -1px rgba(0, 0, 0, 0.2), 0px 6px 10px 0px rgba(0, 0, 0, 0.14),
-				0px 1px 18px 0px rgba(0, 0, 0, 0.12);
+			button {
+				box-shadow: 0px 3px 5px -1px rgba(0, 0, 0, 0.2), 0px 6px 10px 0px rgba(0, 0, 0, 0.14),
+					0px 1px 18px 0px rgba(0, 0, 0, 0.12);
+			}
 		`;
 	}
 };

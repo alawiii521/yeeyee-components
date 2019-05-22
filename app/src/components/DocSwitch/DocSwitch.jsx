a38c926e312c41a9ec6fbef04564cf75097eacc2
Ali <ali.majed.am@hotmail.se>
2019-05-22 20:28:44 +0200

    added label and reducer in DocSwitch

--- app/src/components/DocSwitch/DocSwitch.jsx
+++ app/src/components/DocSwitch/DocSwitch.jsx
@@ -1,12 +1,20 @@
+import PropTypes from 'prop-types';
 import Switch from '../../web-components/Switch';
 import DocSwitchStyle from './DocSwitch.module.less';
+import useSwitchDocReducer from '../../hooks/UseSwitchDocReducer';
 
-function DocSwitch() {
+function DocSwitch(props) {
+	const [value, toggleState] = useSwitchDocReducer(true);
 	return (
 		<div className={DocSwitchStyle.wrapper}>
-			<Switch />
+			<label>{props.label}</label>
+			<Switch checked={value} handleChange={toggleState} />
 		</div>
 	);
 }
+
+DocSwitch.propTypes = {
+	label: PropTypes.string.isRequired,
+};
 
 export default DocSwitch;

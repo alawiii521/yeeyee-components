2b43e6de49340c7e48f073347f77b7bb7d635f37
Ali <ali.majed.am@hotmail.se>
2019-05-22 20:23:22 +0200

    checked prop and handleChange event is added to React Switch Wrapper

--- app/src/web-components/Switch/Switch.jsx
+++ app/src/web-components/Switch/Switch.jsx
@@ -1,4 +1,28 @@
-function Switch() {
-	return <yeeyee-switch />;
+import PropTypes from 'prop-types';
+import { useEffect, useRef } from 'react';
+
+function Switch({ handleChange, checked }) {
+	const inputRef = useRef();
+
+	useEffect(() => {
+		inputRef.current.addEventListener('change', handleChange);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	useEffect(() => {
+		if (checked) {
+			inputRef.current.setAttribute('checked', '');
+		} else {
+			inputRef.current.removeAttribute('checked');
+		}
+	}, [checked]);
+
+	return <yeeyee-switch ref={inputRef} />;
 }
+
+Switch.propTypes = {
+	checked: PropTypes.bool,
+	handleChange: PropTypes.func,
+};
+
 export default Switch;

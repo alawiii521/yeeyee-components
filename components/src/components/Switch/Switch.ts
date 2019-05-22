662b6513a1fec52a897d5a9601953228a7f7b4c4
Ali <ali.majed.am@hotmail.se>
2019-05-22 20:18:54 +0200

    controlled attribute checked for Switch

--- components/src/components/Switch/Switch.ts
+++ components/src/components/Switch/Switch.ts
@@ -3,7 +3,14 @@ import YeeYeeComponent from '../YeeYeeComponent';
 import SwitchStyle from './Switch.style';
 
 class Switch extends YeeYeeComponent {
+	public static CHECKED = 'checked';
+
 	private showRipple: boolean = false;
+	private input: HTMLInputElement = null;
+
+	public static get observedAttributes(): string[] {
+		return [Switch.CHECKED];
+	}
 
 	protected render(): TemplateResult {
 		return html`
@@ -11,7 +18,7 @@ class Switch extends YeeYeeComponent {
 				${SwitchStyle.default}
 			</style>
 			<label class="switch">
-				<input type="checkbox" />
+				<input @input=${(e: Event): void => this.handleInput(e)} type="checkbox" />
 				<span class="slider"></span>
 				<span class="lever"></span>
 				${this.showRipple
@@ -24,24 +31,42 @@ class Switch extends YeeYeeComponent {
 			</label>
 		`;
 	}
-	protected update(): void {}
+
+	protected update(name: string): void {
+		if (name === Switch.CHECKED) {
+			this.getInput().checked = this.hasAttribute(Switch.CHECKED);
+
+			if (this.getInput().checked) {
+				this.startRippleAnimation();
+			}
+		}
+	}
 
 	protected connected(): void {
 		this.litRender();
-		this.shadowRoot.querySelector('input').addEventListener('input', (e: Event): void => this.handleClick(e));
+
+		this.getInput().checked = this.hasAttribute(Switch.CHECKED);
 	}
 
-	private handleClick(e: Event): void {
-		const target = e.target as HTMLInputElement;
+	private handleInput(e: Event): void {
+		this.emit('change', e);
+		this.getInput().checked = this.hasAttribute(Switch.CHECKED);
+	}
 
-		if (target.checked) {
-			this.showRipple = true;
+	private startRippleAnimation(): void {
+		this.showRipple = true;
+		this.litRender();
+		setTimeout((): void => {
+			this.showRipple = false;
 			this.litRender();
-			setTimeout((): void => {
-				this.showRipple = false;
-				this.litRender();
-			}, SwitchStyle.SWITC_RIPPLE_DURATION);
+		}, SwitchStyle.SWITC_RIPPLE_DURATION);
+	}
+
+	public getInput(): HTMLInputElement {
+		if (!this.input) {
+			this.input = this.shadowRoot.querySelector('input');
 		}
+		return this.input;
 	}
 }
 

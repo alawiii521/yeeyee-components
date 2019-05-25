cc04fa1471d5071c8d3f487305a49de310d153c2
Ali <ali.majed.am@hotmail.se>
2019-05-25 18:01:37 +0200

    remove unused css in DrawerItem

--- components/src/components/DrawerItem/DrawerItem.style.ts
+++ components/src/components/DrawerItem/DrawerItem.style.ts
@@ -5,15 +5,13 @@ import { ColorStyle } from '../../style/color.style';
 const width = 250;
 
 const defaultStyle = css`
-	a,
 	div {
 		display: block;
-		padding: ${paddingStyle.large} ${paddingStyle.xxLarge};
 		min-width: ${width.toString()}px;
 		width: ${width.toString()}px;
-		text-decoration: none;
 		color: ${ColorStyle.textColor};
 		font-weight: bold;
+		padding: ${paddingStyle.large} ${paddingStyle.xxLarge};
 		border: solid 0 ${ColorStyle.borderColor};
 		border-bottom-width: 1px;
 		text-align: left;
@@ -21,7 +19,6 @@ const defaultStyle = css`
 		box-sizing: border-box;
 	}
 
-	a:hover,
 	div:hover {
 		background: ${ColorStyle.primary};
 		color: ${ColorStyle.white};

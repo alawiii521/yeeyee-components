db808537bba4cf22ef17e62b02e82c556ea46f71
Ali <ali.majed.am@hotmail.se>
2019-06-27 19:32:56 +0200

    add navigation to Home

--- app/src/pages/Home/Home.jsx
+++ app/src/pages/Home/Home.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import GlobalStyle from '../../style/GlobalStyle';
+import Navigation from '../../components/DocPage/Navigation/Navigation';
 
 class Home extends React.Component {
 	render() {
 		return (
 			<React.Fragment>
 				<GlobalStyle />
-				<yeeyee-navbar name="YeeYee" url="/">
-					<yeeyee-drawer-item destination="https://hd.se">
-						Home
-					</yeeyee-drawer-item>
-					<yeeyee-drawer-item>Sumo</yeeyee-drawer-item>
-					<yeeyee-drawer-item>Menu</yeeyee-drawer-item>
-					<yeeyee-drawer-item>Contact</yeeyee-drawer-item>
-				</yeeyee-navbar>
-				<h1>Hello YeeYee!</h1>
-				<yeeyee-button />
+				<Navigation name={'Home'} />
 			</React.Fragment>
 		);
 	}

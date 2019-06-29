74f7c34bd289bfa66220dc5689d2829f247510c1
Ali <ali.majed.am@hotmail.se>
2019-06-29 22:00:41 +0200

    constants for component names

---
+++ components/src/constants/componentNames.js
@@ -0,0 +1,13 @@
+const ComponentNames = {
+	Button: 'Button',
+	Navbar: 'Navbar',
+	Icon: 'Icon',
+	IconButton: 'IconButton',
+	Drawer: 'Drawer',
+	Overlay: 'Overlay',
+	DrawerItem: 'DrawerItem',
+	NumberInput: 'NumberInput',
+	Switch: 'Switch',
+};
+
+module.exports = ComponentNames;
--- components/webpack/entry.js
+++ components/webpack/entry.js
@@ -1,19 +1,20 @@
 const path = require('path');
+const ComponentNames = require('../src/constants/componentNames');
 
 const BasePath = {
 	YEEYEE_COMPONENTS: path.resolve(__dirname, '..', 'src', 'components'),
 };
 
 const components = {
-	'yeeyee-button': path.resolve(BasePath.YEEYEE_COMPONENTS, 'Button'),
-	'yeeyee-navbar': path.resolve(BasePath.YEEYEE_COMPONENTS, 'Navbar'),
-	'yeeyee-icon': path.resolve(BasePath.YEEYEE_COMPONENTS, 'Icon'),
-	'yeeyee-button-icon': path.resolve(BasePath.YEEYEE_COMPONENTS, 'IconButton'),
-	'yeeyee-drawer': path.resolve(BasePath.YEEYEE_COMPONENTS, 'Drawer'),
-	'yeeyee-overlay': path.resolve(BasePath.YEEYEE_COMPONENTS, 'Overlay'),
-	'yeeyee-drawer-item': path.resolve(BasePath.YEEYEE_COMPONENTS, 'DrawerItem'),
-	'yeeyee-number-input': path.resolve(BasePath.YEEYEE_COMPONENTS, 'NumberInput'),
-	'yeeyee-number-switch': path.resolve(BasePath.YEEYEE_COMPONENTS, 'Switch'),
+	'yeeyee-button': path.resolve(BasePath.YEEYEE_COMPONENTS, ComponentNames.Button),
+	'yeeyee-navbar': path.resolve(BasePath.YEEYEE_COMPONENTS, ComponentNames.Navbar),
+	'yeeyee-icon': path.resolve(BasePath.YEEYEE_COMPONENTS, ComponentNames.Icon),
+	'yeeyee-button-icon': path.resolve(BasePath.YEEYEE_COMPONENTS, ComponentNames.IconButton),
+	'yeeyee-drawer': path.resolve(BasePath.YEEYEE_COMPONENTS, ComponentNames.Drawer),
+	'yeeyee-overlay': path.resolve(BasePath.YEEYEE_COMPONENTS, ComponentNames.Overlay),
+	'yeeyee-drawer-item': path.resolve(BasePath.YEEYEE_COMPONENTS, ComponentNames.DrawerItem),
+	'yeeyee-number-input': path.resolve(BasePath.YEEYEE_COMPONENTS, ComponentNames.NumberInput),
+	'yeeyee-number-switch': path.resolve(BasePath.YEEYEE_COMPONENTS, ComponentNames.Switch),
 };
 
 module.exports = { components };
